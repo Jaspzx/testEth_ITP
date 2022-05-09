d436822a23b3977122b7b7e52e2653d1b34dd654
Jaspzx <jaspzx@hotmail.com>
2022-05-09 14:52:34 +0800

    Created sample Contracts

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,25 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // 
+    mapping(uint => Task) public tasks;
+
+    // Function that is called whenever the smart contract is run for the first time (Deployment).
+    constructor() public{
+        createTask("Get milk.");
+    }
+
+    // Function to create new Task.
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
 }
